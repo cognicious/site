b7c4363dc40cdf2009103387e23cf6c459821722
Daniel Estévez <daniel.ef@gmail.com>
2018-01-09 16:33:26 -0600

    Updates deploy script

--- .ci/deploy.sh
+++ .ci/deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # Decrypt the private key
 # openssl aes-256-cbc -e -in github_deploy_key.pub -out github_deploy_key.enc -K $enc_key -iv $enc_iv
-openssl aes-256-cbc -in .ci/github_deploy_key.enc -out github_deploy_key.pub -K $enc_key -iv $enc_iv -d && \
+openssl aes-256-cbc -d -in .ci/github_deploy_key.enc -out $HOME/github_deploy_key.pub -K $enc_key -iv $enc_iv -d && \
 # Set Git config
-git config core.sshCommand 'ssh -i $PWD/github_deploy_key.pub'
+git config core.sshCommand 'ssh -i $HOME/github_deploy_key.pub' && \
 git config --global user.name "Cognicious Bot" && \
 git config --global user.email 'info@cognicio.us' && \
 # Clone the repository
