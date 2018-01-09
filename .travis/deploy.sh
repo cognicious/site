b4ee37d553f0dc3e7143d7ebaad8cf07b991ff1c
Daniel Estevez <daniel.ef@gmail.com>
2018-01-08 21:48:48 -0600

    Update deploy script

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 # Decrypt the private key
 # See https://github.com/alrra/travis-scripts/blob/master/doc/github-deploy-keys.md
-openssl aes-256-cbc -K $encrypted_502c4417840e_key -iv $encrypted_502c4417840e_iv -in github_deploy_key.enc -out github_deploy_key -d
+openssl aes-256-cbc -K $encrypted_502c4417840e_key -iv $encrypted_502c4417840e_iv -in github_deploy_key.enc -out github_deploy_key -d && \
 # Set the permission of the key
-chmod 600 ~/.ssh/id_rsa
+chmod 600 ~/.ssh/id_rsa && \
 # Start SSH agent
-eval $(ssh-agent)
+eval $(ssh-agent) && \
 # Add the private key to the system
-ssh-add ~/.ssh/id_rsa
+ssh-add ~/.ssh/id_rsa && \
 # Copy SSH config
-cp .travis/ssh_config ~/.ssh/config
+cp .travis/ssh_config ~/.ssh/config && \
 # Set Git config
-git config --global user.name "Cognicious Bot"
-git config --global user.email info@cognicio.us
+git config --global user.name "Cognicious Bot" && \
+git config --global user.email 'info@cognicio.us' && \
 # Clone the repository
-git clone git@github.com:cognicious/cognicious.github.io.git .deploy_git
+git clone git@github.com:cognicious/cognicious.github.io.git .deploy_git && \
 # Deploy to GitHub
 npm run deploy
