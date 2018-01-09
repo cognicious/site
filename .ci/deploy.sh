e85355d5cbeae36f4f952fe1d5820c646eefdfb3
Daniel Estévez <daniel.ef@gmail.com>
2018-01-09 16:42:09 -0600

    Test Codeship

--- .ci/deploy.sh
+++ .ci/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Decrypt the private key
 # openssl aes-256-cbc -e -in github_deploy_key.pub -out github_deploy_key.enc -K $enc_key -iv $enc_iv
-openssl aes-256-cbc -d -in .ci/github_deploy_key.enc -out $HOME/github_deploy_key.pub -K $enc_key -iv $enc_iv -d && \
+openssl aes-256-cbc -in .ci/github_deploy_key.enc -out $HOME/github_deploy_key.pub -K $enc_key -iv $enc_iv -d && \
 # Set Git config
 git config core.sshCommand 'ssh -i $HOME/github_deploy_key.pub' && \
 git config --global user.name "Cognicious Bot" && \
@@ -10,3 +10,4 @@ git config --global user.email 'info@cognicio.us' && \
 git clone git@github.com:cognicious/cognicious.github.io.git .deploy_git && \
 # Deploy to GitHub
 npm run deploy
+
