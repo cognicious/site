b43e015559959b0e940710d1592e1b0ff29c4d97
Daniel Estévez <daniel.ef@gmail.com>
2018-01-09 14:26:34 -0600

    Updates deploy.sh and crypted file

--- .ci/deploy.sh
+++ .ci/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Decrypt the private key
-# openssl aes-256-cbc -e -in github_deploy_key.pub -out github_deploy_key.enc -k $enc_key
-openssl aes-256-cbc -in .ci/github_deploy_key.enc -out ~/.ssh/id_rsa -k $enc_key && \
+# openssl aes-256-cbc -e -in github_deploy_key.pub -out github_deploy_key.enc -K $enc_key -iv $enc_iv
+openssl aes-256-cbc -in .ci/github_deploy_key.enc -out ~/.ssh/id_rsa -K $enc_key -v $enc_iv -d && \
 # Set the permission of the key
 chmod 600 ~/.ssh/id_rsa && \
 # Start SSH agent
