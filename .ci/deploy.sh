3b98cf7a76ad9c3e1e8ed4e07bfc6045b4461cf2
Daniel Estévez <daniel.ef@gmail.com>
2018-01-09 14:16:01 -0600

    Updates deploy.sh and crypted file

--- .ci/deploy.sh
+++ .ci/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Decrypt the private key
-# See https://github.com/alrra/travis-scripts/blob/master/doc/github-deploy-keys.md
-openssl aes-256-cbc -K $encrypted_502c4417840e_key -iv $encrypted_502c4417840e_iv -in .travis/github_deploy_key.enc -out ~/.ssh/id_rsa -d && \
+# openssl aes-256-cbc -e -in github_deploy_key.pub -out github_deploy_key.enc -k $enc_key
+openssl aes-256-cbc -K $enc_key -in .ci/github_deploy_key.enc -out ~/.ssh/id_rsa -d && \
 # Set the permission of the key
 chmod 600 ~/.ssh/id_rsa && \
 # Start SSH agent
@@ -9,7 +9,7 @@ eval $(ssh-agent) && \
 # Add the private key to the system
 ssh-add ~/.ssh/id_rsa && \
 # Copy SSH config
-cp .travis/ssh_config ~/.ssh/config && \
+cp .ci/ssh_config ~/.ssh/config && \
 # Set Git config
 git config --global user.name "Cognicious Bot" && \
 git config --global user.email 'info@cognicio.us' && \
