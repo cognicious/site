649c9ec066fe545a1c543814cc5c65b093f9889a
Daniel Estévez <daniel.ef@gmail.com>
2018-01-11 13:07:03 -0600

    Modifies deploy.sh to use token

--- .ci/deploy.sh
+++ .ci/deploy.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
-# Decrypt the private key
-# openssl aes-256-cbc -e -in github_deploy_key.pub -out github_deploy_key.enc -K $enc_key -iv $enc_iv
-openssl aes-256-cbc -in .ci/github_deploy_key.enc -out $HOME/github_deploy_key.pub -K $enc_key -iv $enc_iv -d && \
-# Set Git config
-git config core.sshCommand 'ssh -i $HOME/github_deploy_key.pub' && \
-git config --global user.name "Cognicious Bot" && \
-git config --global user.email 'info@cognicio.us' && \
-# Clone the repository
-git clone git@github.com:cognicious/cognicious.github.io.git .deploy_git && \
-# Deploy to GitHub
-npm run deploy
 
+# Decrypt personal token
+# - Crypted with: 
+#   openssl aes-256-cbc -e -in github_deploy_key.pub -out github_deploy_key.enc -K $enc_key -iv $enc_iv
+openssl aes-256-cbc -d -in .ci/token.enc -out .ci/token -K $enc_key -iv $enc_iv
+
+# Load token
+TOKEN=$( cat .ci/token )
+
+# Clone with token
+git config --global user.name "Daniel Estevez (bot)"
+git config --global user.email 'daniel.ef@gmail.com'
+git clone https://danielef:$TOKEN@github.com/cognicious/cognicious.github.io.git
+
+# Into cloned copy all generated files
+cd cognicious.github.io/
+cp -r ../public/* .
+
+# Add all changes && commit && push
+git add -A
+DATE=$( date +"%Y-%m-%d %T" )
+git commit -am "Site updated by codeship: $DATE"
+git push
